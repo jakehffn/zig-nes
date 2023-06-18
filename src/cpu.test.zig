6fdaa52170852f8155fe907abec3d1f11c73a654
Jake Hoffman <jaketh19@gmail.com>
2023-06-18 00:59:52 -0400

    Updated testing env gen

--- src/cpu.test.zig
+++ src/cpu.test.zig
@@ -26,102 +26,112 @@ const TestEnv = struct {
 
         return test_env;
     }
+    
+    pub fn write_next(self: *Self, data: []const u8) void {
+        for (data, 0..) |byte, i| {
+            self.bus.write_byte(self.cpu.pc + @intCast(u16, i), byte);
+        }
+    }
+
+    pub fn write_step_expect_a(self: *Self, data: []const u8, expect_a_val: u8) !void {
+        self.write_next(data);
+        self.cpu.step();
+        try expect(self.cpu.a == expect_a_val);
+    }
+
+    pub fn test_with_env(comptime ram_size: usize, init_pc: u16, testFn: *const fn (*TestEnv) anyerror!void) !void {
+        var test_memory = WorkRam(ram_size){};
+        var bus_callback = test_memory.busCallback();
+        var test_env = TestEnv.init(&bus_callback);
+
+        test_env.cpu.pc = init_pc;
+
+        try testFn(&test_env);
+    }
 };
 
-fn write_next(data: []const u8, test_env: *TestEnv) void {
-    for (data, 0..) |byte, i| {
-        test_env.bus.write_byte(test_env.cpu.pc + @intCast(u16, i), byte);
-    }
+test "Immediate" {
+    try TestEnv.test_with_env(0x10, 0x0, struct {
+        pub fn testFn(test_env: *TestEnv) !void {
+            // ADC
+            test_env.cpu.a = 3;
+            try test_env.write_step_expect_a(&[_]u8{0x69, 2}, 5);
+        }
+    }.testFn);
 }
 
 test "ADC" {
-    var test_memory = WorkRam(0x4000){};
-    var bus_callback = test_memory.busCallback();
-    var test_env = TestEnv.init(&bus_callback);
+    try TestEnv.test_with_env(0x4000, 0x2000, struct {
+        pub fn testFn(test_env: *TestEnv) !void {
+            // Immediate
+            test_env.cpu.a = 3;
+            try test_env.write_step_expect_a(&[_]u8{0x69, 2}, 5);
 
-    test_env.cpu.pc = 0x2000;
+            // Zero Page
+            test_env.bus.write_byte(0x0080, 4);
+            try test_env.write_step_expect_a(&[_]u8{0x65, 0x80}, 9);
 
-    // Immediate
-    test_env.cpu.a = 3;
-    write_next(&[_]u8{0x69, 2}, &test_env);
-    test_env.cpu.step();
+            //Zero Page X
+            test_env.cpu.x = 0x20;
+            // 0x20 + 0x60 will refer to the previously written 4 at 0x80
+            try test_env.write_step_expect_a(&[_]u8{0x75, 0x60}, 13);
 
-    try expect(test_env.cpu.a == 5);
+            // Absolute
+            test_env.bus.write_byte(0x1234, 20);
+            try test_env.write_step_expect_a(&[_]u8{0x6D, 0x34, 0x12}, 33);
 
-    // Zero Page
-    test_env.bus.write_byte(0x0080, 4);
-    write_next(&[_]u8{0x65, 0x80}, &test_env);
-    test_env.cpu.step();
+            // Absolute X
+            test_env.cpu.x = 0x34;
+            // 0x1200 + 0x34 will refer to the previously written 20 at 0x1234
+            try test_env.write_step_expect_a(&[_]u8{0x7D, 0x00, 0x12}, 53);
+        
+            // Absolute Y
+            test_env.cpu.y = 0x34;
+            // 0x1200 + 0x34 will refer to the previously written 20 at 0x1234
+            try test_env.write_step_expect_a(&[_]u8{0x79, 0x00, 0x12}, 73);
 
-    try expect(test_env.cpu.a == 9);
+            // Indexed Indirect
+            test_env.cpu.x = 0x0F;
+            test_env.bus.write_byte(0x0010, 0x34);
+            test_env.bus.write_byte(0x0011, 0x12);
+            try test_env.write_step_expect_a(&[_]u8{0x61, 0x01}, 93);
 
-    //Zero Page X
-    test_env.cpu.x = 0x20;
-    // 0x20 + 0x60 will refer to the previously written 4 at 0x80
-    write_next(&[_]u8{0x75, 0x60}, &test_env);
-    test_env.cpu.step();
+            // Indirect Indexed
+            test_env.cpu.y = 0xFF;
+            test_env.bus.write_byte(0x0010, 0x35);
+            test_env.bus.write_byte(0x0011, 0x11);
+            // 0x35 + 0xFF = 0x34 + C
+            // 0x11 + C = 0x12
+            // $0x1234 = 20
+            try test_env.write_step_expect_a(&[_]u8{0x71, 0x10}, 113);
 
-    try expect(test_env.cpu.a == 13);
+            // Flags
 
-    // Absolute
-    test_env.bus.write_byte(0x1234, 20);
-    write_next(&[_]u8{0x6D, 0x34, 0x12}, &test_env);
-    test_env.cpu.step();
+            // Negative and overflow
+            test_env.cpu.a = 0b0111_1111;
+            test_env.write_next(&[_]u8{0x69, 1});
+            test_env.cpu.step();
 
-    try expect(test_env.cpu.a == 33);
+            try expect(test_env.cpu.flags.N == 1);
+            try expect(test_env.cpu.flags.V == 1);
 
-    // Absolute X
-    test_env.cpu.x = 0x34;
-    // 0x1200 + 0x34 will refer to the previously written 20 at 0x1234
-    write_next(&[_]u8{0x7D, 0x00, 0x12}, &test_env);
-    test_env.cpu.step();
+            // Zero and carry
+            test_env.cpu.a = 0xFF;
+            test_env.write_next(&[_]u8{0x69, 1});
+            test_env.cpu.step();
 
-    try expect(test_env.cpu.a == 53);
- 
-    // Absolute Y
-    test_env.cpu.y = 0x34;
-    // 0x1200 + 0x34 will refer to the previously written 20 at 0x1234
-    write_next(&[_]u8{0x79, 0x00, 0x12}, &test_env);
-    test_env.cpu.step();   
+            try expect(test_env.cpu.flags.Z == 1);
+            try expect(test_env.cpu.flags.C == 1);
+        }
+    }.testFn);
+}
 
-    try expect(test_env.cpu.a == 73);
-
-    // Indexed Indirect
-    test_env.cpu.x = 0x0F;
-    test_env.bus.write_byte(0x0010, 0x34);
-    test_env.bus.write_byte(0x0011, 0x12);
-    write_next(&[_]u8{0x61, 0x01}, &test_env);
-    test_env.cpu.step();
-
-    try expect(test_env.cpu.a == 93);
-
-    // Indirect Indexed
-    test_env.cpu.y = 0xFF;
-    test_env.bus.write_byte(0x0010, 0x35);
-    test_env.bus.write_byte(0x0011, 0x11);
-    // 0x35 + 0xFF = 0x34 + C
-    // 0x11 + C = 0x12
-    // $0x1234 = 20
-    write_next(&[_]u8{0x71, 0x10}, &test_env);
-    test_env.cpu.step();
-
-    try expect(test_env.cpu.a == 113);
-
-    // Flags
-
-    // Negative and overflow
-    test_env.cpu.a = 0b0111_1111;
-    write_next(&[_]u8{0x69, 1}, &test_env);
-    test_env.cpu.step();
-
-    try expect(test_env.cpu.flags.N == 1);
-    try expect(test_env.cpu.flags.V == 1);
-
-    // Zero and carry
-    test_env.cpu.a = 0xFF;
-    write_next(&[_]u8{0x69, 1}, &test_env);
-    test_env.cpu.step();
-
-    try expect(test_env.cpu.flags.Z == 1);
-    try expect(test_env.cpu.flags.C == 1);
+test "AND" {
+    try TestEnv.test_with_env(0x20, 0x10, struct {
+        pub fn testFn(test_env: *TestEnv) !void {
+            // Immediate
+            test_env.cpu.a = 0b1111_0000;
+            try test_env.write_step_expect_a(&[_]u8{0x29, 0b0011_0011}, 0b0011_0000);
+        }
+    }.testFn);
 }
