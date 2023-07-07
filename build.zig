3bbde1a70556b83525ec6ee84ef837e8e6acbfe4
Jake Hoffman <jaketh19@gmail.com>
2023-07-07 17:31:12 -0400

    cimgui build working

--- build.zig
+++ build.zig
@@ -17,7 +17,7 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize
     });
-    imgui.linkLibC();
+    imgui.linkLibCpp();
 
     imgui.addCSourceFiles( 
         &[_][]const u8{
@@ -30,24 +30,43 @@ pub fn build(b: *std.Build) void {
             "libs/cimgui/imgui/backends/imgui_impl_opengl3.cpp",
             "libs/cimgui/imgui/imgui_widgets.cpp",
         },
-        &[_][]const u8{"-std=c++17"}
+        &[_][]const u8{
+            "-std=c++17", 
+            // "-DCIMGUI_DEFINE_ENUMS_AND_STRUCTS",
+            "-DIMGUI_IMPL_API=extern \"C\" __declspec(dllexport)",
+            "-DCIMGUI_USE_SDL2",
+            "-DCIMGUI_USE_OPENGL3"
+        }
     );
+    const sdl_path = "C:/lib/SDL2-2.26.4/";
 
     imgui.addIncludePath("./libs/cimgui/imgui");
+    imgui.addIncludePath(sdl_path ++ "include");
+    imgui.addLibraryPath(sdl_path ++ "lib/x64");
+
+    imgui.addIncludePath("C:/msys64/mingw64/include");
+    imgui.addLibraryPath("C:/msys64/mingw64/lib");
     
-    const sdl_path = "C:/lib/SDL2-2.26.4/";
     // imgui also needs sdl to compile
     imgui.addIncludePath(sdl_path ++ "include");
+    imgui.linkSystemLibrary("opengl32");
+    imgui.linkSystemLibrary("sdl2");
+    // b.installArtifact(imgui);
 
     exe.addIncludePath("./libs/cimgui");
-    exe.addIncludePath("./libs/cimgui/imgui");
+    exe.addIncludePath("./libs/cimgui/generator/output");
     exe.linkLibrary(imgui);
 
     exe.addIncludePath(sdl_path ++ "include");
     exe.addLibraryPath(sdl_path ++ "lib/x64");
     b.installBinFile(sdl_path ++ "lib/x64/SDL2.dll", "SDL2.dll");
 
+    exe.addIncludePath("C:/msys64/mingw64/include");
+    exe.addLibraryPath("C:/msys64/mingw64/lib");
+
+    exe.linkSystemLibrary("opengl32");
     exe.linkSystemLibrary("sdl2");
+    exe.linkSystemLibrary("glew32");
     exe.linkLibC();
     b.installArtifact(exe);
 
