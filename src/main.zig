f4fd8f9d9a6dda0dce3678492812fde038015e42
Jake Hoffman <jaketh19@gmail.com>
2024-01-02 12:32:28 -0500

    Removed wasm

--- src/main.zig
+++ src/main.zig
@@ -2,10 +2,8 @@ const builtin = @import("builtin");
 
 const Emulator = @import("./core/emulator.zig");
 
-pub const Frontend = switch (builtin.cpu.arch) {
-    //.wasm32, .wasm64 => @import();
-    else => @import("./frontends/sdl_imgui/sdl_imgui.zig")
-};
+pub const Frontend = @import("./frontends/sdl_imgui/sdl_imgui.zig");
+
 var emulator: Emulator = .{};
 
 pub fn main() !void {
